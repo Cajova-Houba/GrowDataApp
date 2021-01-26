5df24d32bb4970e4ab01bbd517fc31d9fd66c602
Cajova Houba <tkmushroomer@gmail.com>
2021-01-26 23:43:47 +0100

    Webapp now fetches data from the server

--- Webapp/js/app.js
+++ Webapp/js/app.js
@@ -1,34 +1,59 @@
+
 function showData() {
-    var tmpCtx = document.getElementById('temperatureChart').getContext('2d');
-    var humCtx = document.getElementById('humidityChart').getContext('2d');
-    dataSubset = growData.filter(obj => obj["timestamp"].startsWith("2021-01-25"));
-    var tmpChart = new Chart(tmpCtx, {
+    fetch('http://69.90.132.97:8666/api/growdata')
+        .then(response => response.json())
+        .then(growData => {
+            var tmpCtx = document.getElementById('temperatureChart').getContext('2d');
+            var humCtx = document.getElementById('humidityChart').getContext('2d');
+            dataSubset = growData.filter(obj => obj["timestamp"].startsWith("2021-01-25"));
+            var tmpChart = createTemperatureChart(tmpCtx, dataSubset);
+
+            var humChart = createHumidityChart(humCtx, dataSubset);
+        });
+}
+
+/**
+ * Returns a Chart configuration for plotting temperature.
+ * 
+ * @param {*} ctx Context to draw on.
+ * @param {Array} growData Data to plot.
+ */
+function createTemperatureChart(ctx, growData) {
+    return new Chart(ctx, {
         type: 'line',
         data: {
-            labels: dataSubset.map(d => d["timestamp"].substr(11)),
+            labels: growData.map(d => d["timestamp"].substr(11)),
             datasets: [{
                 label: "Soil temperature [C°]",
-                data: dataSubset.map(d => d["soilTemperature"]),
+                data: growData.map(d => d["soilTemperature"]),
                 fill: false,
                 pointRadius: 0,
                 borderColor: "red"
             },{
                 label: "Air temperature [C°]",
-                data: dataSubset.map(d => d["airTemperature"]),
+                data: growData.map(d => d["airTemperature"]),
                 fill: false,
                 pointRadius: 0,
                 borderColor: "blue"
             }]
         }
     });
+}
 
-    var humChart = new Chart(humCtx, {
+/**
+ * Returns a Chart configuration for plotting humidity.
+ * 
+ * @param {*} ctx Context to draw on.
+ * @param {Array} growData Data to plot.
+ */
+function createHumidityChart(ctx, growData) {
+    return new Chart(ctx, {
         type: 'line',
         data: {
-            labels: dataSubset.map(d => d["timestamp"].substr(11)),
+            labels: growData.map(d => d["timestamp"].substr(11)),
             datasets: [{
                 label: "Air humidity [%]",
-                data: dataSubset.map(d => d["airHumidity"]),
+                data: growData.map(d => d["airHumidity"]),
                 fill: false,
                 pointRadius: 0,
                 borderColor: "green"
