e9ec577636d42c605597b6f4e6762d65841f01c7
Cajova Houba <tkmushroomer@gmail.com>
2021-03-06 17:45:52 +0100

    #1: Health check endpoint added.

---
+++ Server/App/Controllers/HealthController.cs
@@ -0,0 +1,32 @@
+﻿using GrowDataApp.Core.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrowDataApp.App.Controllers
+{
+    /// <summary>
+    /// Controller used for health checks.
+    /// </summary>
+    [Route("/health")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        private readonly ILogger<HealthController> _logger;
+
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public HealthCheck Health()
+        {
+            _logger.LogInformation("Health check OK.");
+            return new HealthCheck();
+        }
+    }
+}
---
+++ Server/Core/Model/HealthCheck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrowDataApp.Core.Model
+{
+    /// <summary>
+    /// Small entity returned as a result of health check.
+    /// </summary>
+    public class HealthCheck
+    {
+        public string Status { get; private set; }
+
+        public HealthCheck()
+        {
+            Status = "OK";
+        }
+    }
+}
