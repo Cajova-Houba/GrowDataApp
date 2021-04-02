9e36755c991e258f6bd58dc7ee644b793b6e50dc
Cajova Houba <tkmushroomer@gmail.com>
2021-04-02 17:27:02 +0200

    #4: New API used in webapp. Bootstrap styling added.

--- Webapp/js/app.js
+++ Webapp/js/app.js
@@ -1,14 +1,17 @@
 
 function showData() {
-    fetch('http://69.90.132.97:8666/api/growdata')
+    const from = document.getElementById('from').value+'T12:00:00';
+    const to = document.getElementById('to').value+'T12:00:00';
+
+    console.log(`Fetching data from ${from} to ${to}`);
+
+    fetch(`http://69.90.132.97:8666/api/growdata?from=${from}&to=${to}`)
         .then(response => response.json())
         .then(growData => {
             var tmpCtx = document.getElementById('temperatureChart').getContext('2d');
             var humCtx = document.getElementById('humidityChart').getContext('2d');
-            dataSubset = growData.filter(obj => obj["timestamp"].startsWith("2021-01-25"));
-            var tmpChart = createTemperatureChart(tmpCtx, dataSubset);
-
-            var humChart = createHumidityChart(humCtx, dataSubset);
+            var tmpChart = createTemperatureChart(tmpCtx, growData);
+            var humChart = createHumidityChart(humCtx, growData);
         });
 }
 
@@ -22,7 +25,7 @@ function createTemperatureChart(ctx, growData) {
     return new Chart(ctx, {
         type: 'line',
         data: {
-            labels: growData.map(d => d["timestamp"].substr(11)),
+            labels: growData.map(createDateTimeLabel),
             datasets: [{
                 label: "Soil temperature [C°]",
                 data: growData.map(d => d["soilTemperature"]),
@@ -50,7 +53,7 @@ function createHumidityChart(ctx, growData) {
     return new Chart(ctx, {
         type: 'line',
         data: {
-            labels: growData.map(d => d["timestamp"].substr(11)),
+            labels: growData.map(createDateTimeLabel),
             datasets: [{
                 label: "Air humidity [%]",
                 data: growData.map(d => d["airHumidity"]),
@@ -60,4 +63,13 @@ function createHumidityChart(ctx, growData) {
             }]
         }
     });
+}
+
+/**
+ * Creates datetime label for given data item.
+ * 
+ * @param {Object} dataItem Data item to create datetime label for.
+ */
+function createDateTimeLabel(dataItem) {
+    return dataItem["timestamp"].substr(0,10);
 }
